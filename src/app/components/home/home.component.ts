74da81c0613f930a395717b486908d9d9ac9ae74
Saptarshi Datta <d.saptarshi2001@gmail.com>
2024-10-30 16:12:04 +0530

    stable, add the validators later

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
 import { Role } from '../../Models/roles.enum';
 
 @Component({
@@ -7,17 +6,10 @@ import { Role } from '../../Models/roles.enum';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit  {
+export class HomeComponent{
   isLoggedIn = false;
   isAdmin = false;
 
-  constructor(private authService: AuthService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.authService.userStatus$.subscribe((currentUser) => {
-      this.isLoggedIn = !!currentUser; // Update login status
-      this.isAdmin = currentUser?.role === Role.Admin; // Check if the user is an admin
-      console.log(currentUser); // Should log the current user if logged in
-    });
-  }
 }
